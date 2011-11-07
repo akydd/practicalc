c1ac0714e19fe40ac42e96547a75efd362b5130b
akydd <akydd@ualberta.net>
2011-11-06 22:17:08 -0700

    started chapter 6

---
+++ c6/1.c
@@ -0,0 +1,59 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  1.c
+ *
+ *    Description:  Convert Imperial units to metric
+ *
+ *        Version:  1.0
+ *        Created:  11-11-06 09:43:45 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Alan Kydd (), akydd@ualberta.net
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char input[10];
+	int option;
+	float imp_value;
+
+	float conversions[] = { 2.54, 0.3048, 0.9144, 1.6093, \
+		0.56038, 4.5461, 28.35, 0.4536, 6.3503 };
+
+	while (1)
+	{
+		(void) printf("To convert, type the menu option followed by ");
+		(void) printf("the value you wish to convert\n");
+		(void) printf(" 1. inches to centimeters\n");
+		(void) printf(" 2. feet to meters\n");
+		(void) printf(" 3. yards to meters\n");
+		(void) printf(" 4. miles to kilometers\n");
+		(void) printf(" 5. pints to liters\n");
+		(void) printf(" 6. gallons to liters\n");
+		(void) printf(" 7. ounces to grams\n");
+		(void) printf(" 8. pounds to kilograms\n");
+		(void) printf(" 9. stone to kilograms\n");
+		(void) printf("Type '0' to quit.\n");
+
+		(void) fgets(input, sizeof(input), stdin);
+		(void) sscanf(input, "%d %f", &option, &imp_value);
+
+		if (option == 0)
+			break;
+
+		if ((option <= 9) && (option >= 0))
+			(void) printf("%f converts to %f.\n\n", imp_value, imp_value * conversions[option-1]);
+		else
+			(void)printf("Invalid option.\n\n");
+	}
+
+	(void) printf("Bye!\n");
+	return 0;
+}
---
+++ c6/2.c
@@ -0,0 +1,77 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  2.c
+ *
+ *    Description:  Find the # days between two after epoch calendar dates.
+ *    		    Dates must be fomatted as YYYY-MM-DD
+ *
+ *        Version:  1.0
+ *        Created:  11-11-06 02:44:53 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Alan Kydd (), akydd@ualberta.net
+ *        Company:  
+ *
+ * =============================================================================
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+int main(void)
+{
+	char input[20];
+	char input1[20];;
+	const char *d1, *d2;
+
+	struct tm tm_1, tm_2;
+
+	int diff;
+
+	(void) printf("Enter first date (YYYY-MM-DD): ");
+	(void) fgets(input, sizeof(input), stdin);
+	/*  strip newline */
+	input[(strlen(input))-1] = '\0';
+	d1 = strptime(input, "%F", &tm_1);
+
+	(void) printf("Enter second date (YYYY-MM-DD): ");
+	(void) fgets(input1, sizeof(input1), stdin);
+	/* strip newline */
+	input[(strlen(input1))-1] = '\0';
+	d2 = strptime(input1, "%F", &tm_2);
+
+	/* Checking here is tricky...
+	 * strptime returns NULL pointer if no input fields can be matched,
+	 * but will point to a non \0 char if there is only a partial match.
+	 * 
+	 * We want a complete match, so we check both conditions, but we
+	 * have to check for the NULL pointer first, otherwise we risk a seg
+	 * fault for trying to check if the  pointer (which could be NULL) is
+	 * pointing to a non \0 character.
+	 */
+	if ((d1 == NULL) || (*d1 != '\0')) {
+		printf("The first input date is invalid.\n");
+	} else if ((d2 == NULL) || (*d1 != '\0')) {
+		printf("The second input date is invalid.\n");
+	} else {
+		(void) printf("0x%x\n", *d1);
+		/*  calculate days diff based on year and days since jan 1 */
+		if (tm_1.tm_year > tm_2.tm_year) {
+			diff = (tm_1.tm_year - tm_2.tm_year) * 365 + \
+			       tm_1.tm_yday - tm_2.tm_yday;
+		} else if (tm_2.tm_year > tm_1.tm_year) {
+			diff = (tm_2.tm_year - tm_1.tm_year) * 365 + \
+			       tm_2.tm_yday - tm_1.tm_yday;
+		} else {
+			diff = abs(tm_1.tm_yday - tm_2.tm_yday);
+		}
+
+		(void) printf("There are %d days between.\n", diff);
+	}
+
+	return 0;
+}
