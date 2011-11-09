90299cb6ef541bd0179bce3bea6531f8869b692a
akydd <akydd@ualberta.net>
2011-11-08 19:40:58 -0700

    Finishsed #3.  Works with gcc, not lc (for win32)

--- c6/3.c
+++ c6/3.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
 #ifndef CHAR_BIT
-#define CHAR_BIT 8.0
+#define CHAR_BIT 8
 #endif
 
-int size;
-int size_unit;
-int time_unit;
+static int size;
+static int size_unit;
+static int time_unit;
+
+static void set_filesize(void);
+static void set_size_unit(void);
+static void set_time_unit(void);
+static float convert_time(float);
+static float size_to_bits(void);
 
 int main(void)
 {
-	const long double bits_per_sec = 960.0 * CHAR_BIT;
+	const float bits_per_sec = (float)(960.0 * CHAR_BIT);
 
-	long double time_in_secs;
-	long double converted_time;
-	long double size_in_bits;
+	float time_in_secs;
+	float converted_time;
+	float size_in_bits;
 
 	/* Set the filesize, size units, and time units	 */
 	set_filesize();
@@ -22,38 +28,38 @@ int main(void)
 	set_time_unit();
 
 	/* convert filesize to bits */
-	size_in_bits = (long double)size_to_bits();
+	size_in_bits = size_to_bits();
 
 	if (size_in_bits != -1) {
-		(void)printf("Size in bits is %10.2Lf.\n", size_in_bits);
-		(void)printf("2: size_in_bits is %l.\n", size_to_bits());
 		/* time to process, in seconds */
 		time_in_secs = size_in_bits / bits_per_sec;
 
 		/* convert the time.  If valid, print it out. */
 		converted_time = convert_time(time_in_secs);
 		if (converted_time != -1) {
-			(void)printf("Total time is %Lf.\n", converted_time);
+			(void)printf("Total time is %f.\n", converted_time);
 		} else {
-			(void)printf("Correct errors and try again.\n");
+			(void)printf("Correct a valid time and try again.\n");
 		}
+	} else {
+		(void)printf("Choose a valid size and try again.\n");
 	}
 
 	return 0;
 }
 
-long double convert_time(long double seconds)
+float convert_time(float seconds)
 {
-	long double time = -1;
+	float time = -1;
 
 	if (time_unit == 1) {
 		time = seconds;
 	} else if (time_unit == 2) {
-		time = time / 60.0;
+		time = (float)(seconds / 60);
 	} else if (time_unit == 3) {
-		time = (time / 60.0) / 60.0;
+		time = (float)((seconds / 60.0) / 60.0);
 	} else if (time_unit == 4) {
-		time = ((time / 60.0) / 60.0) / 24.0;
+		time = (float)(((seconds / 60.0) / 60.0) / 24.0);
 	} else {
 		(void)printf("Invalid time unit!\n");
 	}
@@ -61,22 +67,24 @@ long double convert_time(long double seconds)
 	return time;
 }
 
-long double size_to_bits(void)
+float size_to_bits(void)
 {
-	long double size_in_bits = -1;
+	float size_in_bits = -1;
 
-	if (size_unit == 1) {
-		size_in_bits = (long double)size;
-	} else if (size_unit == 2) {
-		size_in_bits = (long double)size * 8;
-	} else if (size_unit == 3) {
-		size_in_bits = (long double)size * 8 * 1048576;
-	} else {
-		(void)printf("Invalid size unit!\n");
+	/*  we only like non negative sizes */
+	if (size >= 0) {
+		if (size_unit == 1) {
+			size_in_bits = (float)size;
+		} else if (size_unit == 2) {
+			size_in_bits = (float)size * 8;
+		} else if (size_unit == 3) {
+			size_in_bits = (float)size * 8 * 1048576;
+		} else {
+			(void)printf("Invalid size unit!\n");
+		}
 	}
 
-	(void)printf("size_in_bits is %Lf.\n", size_in_bits);
-	return (size_in_bits);
+	return size_in_bits;
 }
 
 void set_filesize(void)
@@ -86,7 +94,7 @@ void set_filesize(void)
 
 	do {
 		(void)printf("Enter size: ");
-		fgets(str, sizeof(str), stdin);
+		(void)fgets(str, sizeof(str), stdin);
 		result = sscanf(str, "%d", &size);
 	} while (result != 1);
 }
@@ -101,7 +109,7 @@ void set_size_unit(void)
 		(void)printf(" 1. bits\n");
 		(void)printf(" 2. bytes\n");
 		(void)printf(" 3. megabytes\n");
-		fgets(str, sizeof(str), stdin);
+		(void)fgets(str, sizeof(str), stdin);
 		result = sscanf(str, "%d", &size_unit);
 	} while (result != 1);
 }
@@ -117,7 +125,7 @@ void set_time_unit(void)
 		(void)printf(" 2. minutes\n");
 		(void)printf(" 3. hours\n");
 		(void)printf(" 4. days\n");
-		fgets(str, sizeof(str), stdin);
+		(void)fgets(str, sizeof(str), stdin);
 		result = sscanf(str, "%d", &time_unit);
 	} while (result != 1);
 }
