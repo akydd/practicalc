9d73a2b22b9411c2b7b2f75f4048808de994cfba
Alan Kydd <akydd@ualberta.net>
2012-02-24 20:15:20 -0700

    13.4b done

---
+++ c13/4b.c
@@ -0,0 +1,61 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  4b.c
+ *
+ *    Description:  Converts a binary file of numbers to an ASCII file
+ *
+ *        Version:  1.0
+ *        Created:  12-02-24 07:50:14 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Alan Kydd (), akydd@ualberta.net
+ *        Company:  
+ *
+ * =============================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+static void usage(void);
+
+int main(int argc, char *argv[])
+{
+	char *in_filename = argv[1];
+	char *out_filename = argv[2];
+
+	FILE *in_file;
+	FILE *out_file;
+
+	int number;
+
+       	in_file = fopen(in_filename, "r");
+	out_file = fopen(out_filename, "w");
+
+	/* did files open ok? */
+	if (in_file == NULL) {
+		(void)printf("Could not open input file!\n");
+		usage();
+	}
+	if (out_file == NULL) {
+		(void)printf("Could not open output file!\n");
+		usage();
+	}
+
+	while (fread(&number, sizeof(int), 1, in_file) != 0) {
+		(void)fprintf(out_file, "%d\n", number);
+	}
+
+	/* close all files */
+	(void)fclose(out_file);
+	(void)fclose(in_file);
+
+	return 0;
+}
+
+void usage()
+{
+	(void)printf("Use this program properly!\n");
+	exit(EXIT_FAILURE);
+}
