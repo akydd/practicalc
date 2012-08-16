da6e123850870ae4d7e4ae86248895ce783090b5
Alan Kydd <akydd@ualberta.net>
2012-08-15 19:38:59 -0600

    Much improved functions to count POSIX words in a string

--- c8/1.c
+++ c8/1.c
@@ -4,9 +4,8 @@
  *       Filename:  1.c
  *
  *    Description:  wordcount counts the # of words in a string.
- *    		    I defined a word as a sequence of at least 1 chars,
- *    		    each having dec value in [65, 90] U [97, 122]. Words must
- *    		    be separated by whitespace or newline
+ *    		    I defined a word using the POSIX character class:
+ *    		    [A-Za-z0-9_]
  *
  *        Version:  1.0
  *        Created:  11-11-20 10:18:26 AM
@@ -22,49 +21,33 @@
 #include <ctype.h>
 
 static int wordcount(char*);
+static int isword(char);
+
+int isword(char character)
+{
+	if ((isalnum((int)character) != 0) || (character == '_')) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
 
 int wordcount(char *string)
 {
 	int count = 0;
 	char *str_ptr = string;
-	char *srch_ptr = str_ptr + 1;
-	char *parse_ptr;
-	int increment = 1;
+	int is_previous_word = 0;
 
 	while (*str_ptr != '\0') {
-
-		/* skip leading whitespace */
-		while ((isspace((int)*str_ptr)) != 0) {
-			str_ptr++;
-		}
-
-		/* find the 1st non whitespace char */
-		if (isspace((int)*str_ptr) == 0) {
-			/* then find the next whitespace, or \0 */
-			srch_ptr = str_ptr + 1;
-			while ( (*srch_ptr != '\0') && 
-					(isspace((int)*srch_ptr) == 0) ) {
-				srch_ptr++;
-			}
-
-			/* check all chars between the two ptrs */
-			for (parse_ptr = str_ptr; parse_ptr < srch_ptr;
-					parse_ptr++) {
-				/* if char is not alpha, don't increment
-				 * count, and forget about the other chars */
-				if(isalpha((int)*parse_ptr) == 0) {
-					increment = 0;
-					break;
-				}
-			}
-
-			if (increment == 1) {
+		if(isword(*str_ptr) != 0) {
+			if(is_previous_word == 0) {
 				count++;
 			}
+			is_previous_word = 1;
+		} else {
+			is_previous_word = 0;
 		}
-
-		/* move str_ptr to trailing whitespace or \0 */
-		str_ptr = srch_ptr;
+		str_ptr++;
 	}
 	return count;
 }
